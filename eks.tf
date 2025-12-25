b2df48d14a3c6fb16fe3bffaa2573ab4f5a8ef37
Adnane El Harti <adnanelharti03@email.com>
2025-12-25 13:36:37 +0100

    added managed cluster module in eks

--- eks.tf
+++ eks.tf
@@ -17,7 +17,7 @@ module "eks" {
   eks_managed_node_groups = {
     spot_workers = {
       # Isti3mal Spot Instances bach t-nqqss 90% d l-taman
-      capacity_type  = "SPOT" 
+      capacity_type  = "SPOT"
       instance_types = ["t3.medium", "t3.small"] # Diversification bach t-fada interruption
 
       min_size     = 1
@@ -25,6 +25,7 @@ module "eks" {
       desired_size = 2
 
       # Tags daroryin l-AWS
+      
       labels = {
         role = "spot-worker"
       }
@@ -39,3 +40,54 @@ module "eks" {
     Terraform   = "true"
   }
 }
+
+
+module "eks_spot_nodes" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = module.eks_cluster.cluster_name
+  cluster_version = "1.30"
+
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    spot_workers = {
+      capacity_type  = "SPOT"
+      instance_types = ["t3.medium", "t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      labels = {
+        role = "spot-worker"
+      }
+    }
+
+  }
+
+  depends_on = [module.eks_cluster]
+}
+
+/*
+module "eks_cluster" {
+source = "terraform-aws-modules/eks/aws"
+version = "~> 20.0"
+
+cluster_name = "pfe-spot-cluster"
+cluster_version = "1.30"
+
+vpc_id = module.vpc.vpc_id
+subnet_ids = module.vpc.private_subnets
+control_plane_subnet_ids = module.vpc.private_subnets
+
+cluster_endpoint_public_access = true
+
+enable_cluster_creator_admin_permissions = true
+
+tags = {
+Environment = "pfe"
+Terraform = "true"
+}
+} */
